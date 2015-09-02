db652404cfbd617647280daa92489d6193710691
Brent Atkinson <brent.atkinson@gmail.com>
2015-09-02 19:41:14 -0400

    Removed unused import.

--- src/main/java/com/github/cims_bioko/idscheme_test/IndexImpl.java
+++ src/main/java/com/github/cims_bioko/idscheme_test/IndexImpl.java
@@ -26,7 +26,6 @@ import org.apache.lucene.store.NoSuchDirectoryException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
